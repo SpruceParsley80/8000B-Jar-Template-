22ac74d32758fa4f627cf406c1901888e200b420
SpruceParsley80 <slugbug427@gmail.com>
2025-11-18 17:36:15 -0800

    cp (carlo particles)

--- include/JAR-Template/odom.h
+++ include/JAR-Template/odom.h
@@ -18,7 +18,7 @@ public:
   void set_position(float X_position, float Y_position, float orientation_deg, float ForwardTracker_position, float SidewaysTracker_position);
   void update_position(float ForwardTracker_position, float SidewaysTracker_position, float orientation_deg);
   void set_physical_distances(float ForwardTracker_center_distance, float SidewaysTracker_center_distance);
-  float sampleY();
+  float sampleY(); //s
   float sampleX();
   float odominator(float x, float y);
   void actualOdominator(float targetX, float targetY);
